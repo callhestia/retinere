4bc789c548ad208972f077683ef64dd8336e1a14
fran <user@pop-os.tail3d114d.ts.net>
2026-05-17 18:05:11 +0200

    fix: ocena 0 nie zmienia wspolczynnika EF

--- src/engine/sm2.cpp
+++ src/engine/sm2.cpp
@@ -5,11 +5,11 @@ double zaplanujPowtorke(int ocena, double poprzedniWspolczynnik) {
 
     double wspolczynnik = 0.0;
 
-    if (ocena > 3) {
+    if (ocena == 0) {
+        wspolczynnik = poprzedniWspolczynnik;
+    } else if (ocena > 3) {
         wspolczynnik = poprzedniWspolczynnik + (0.1 - (5 - ocena) * (0.08 + (5 - ocena) * 0.02));
-    } 
-    
-    else {
+    } else {
         wspolczynnik = poprzedniWspolczynnik - 0.15 - (3 - ocena) * 0.05;
     }
 
